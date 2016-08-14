587e1a62cf65469dddee3e0eec09cef43b54aa60
Chris Rowe <chriz001@gmail.com>
2016-08-14 23:03:36 +1000

    camera with crosshair

--- index.android.js
+++ index.android.js
@@ -7,68 +7,49 @@
 import React, { Component } from 'react'
 import {
   AppRegistry,
+  Dimensions,
   StyleSheet,
   Text,
   View,
-  TouchableNativeFeedback
+  Image
 } from 'react-native'
+import Camera from 'react-native-camera'
 
 class kizza extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      count: 0
-    }
-  }
   render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native Kieran!
-        </Text>
-        <TouchableNativeFeedback
-          onPress={this.buttonClicked.bind(this)}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>{`Pressed ${this.state.count} times!`}</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Image
+            source={require('./crosshair.png')}
+            style={styles.crosshair} />
+        </Camera>
       </View>
     )
-  }
-  buttonClicked () {
-    this.setState({count: this.state.count + 1})
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  preview: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  },
-  button: {
-    marginBottom: 7,
-    backgroundColor: '#ff0000',
-    padding: 10,
-    borderRadius: 2
-  },
-  buttonText: {
-    color: '#ffffff'
+  crosshair: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'transparent',
+    marginRight: 10
   }
 })
 
